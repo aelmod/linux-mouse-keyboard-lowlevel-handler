ff665a4c8b5c749672c2537f4e4f06c51dadef67
aelmod <aelmod@gmai.com>
2018-01-09 18:15:59 +0200

    my first commit

--- main.c
+++ main.c
@@ -32,35 +32,24 @@ int main(int argc, char *argv[]) {
     int maxfd;
 
     DIR *dirp;
-    static char *dirName = "/dev/input";
+    static char *dirName = "/dev/input/by-id";
     char fullPath[1024];
     struct dirent *dp;
     regex_t kbd;
 
-    if ((dirp = opendir(dirName)) == NULL) {
-        die("couldn't open");
+    if (regcomp(&kbd, "event-kbd", 0) != 0) {
+        die("regcomp for kbd failed");
     }
 
-    if (regcomp(&kbd, "event", 0) != 0) {
-        printf("regcomp for kbd failed\n");
-        die("");
+    if ((dirp = opendir(dirName)) == NULL) {
+        die("couldn't open '/dev/input/by-id'");
     }
 
     do {
         if ((dp = readdir(dirp)) != NULL) {
             if (regexec(&kbd, dp->d_name, 0, NULL, 0) == 0) {
-//                printf("match for the event = %s\n", dp->d_name);
                 sprintf(fullPath, "%s/%s", dirName, dp->d_name);
-                int tmp = open(fullPath, O_RDONLY | O_NONBLOCK);
-                char tmpName[256];
-                memset(tmpName, 0, sizeof(tmpName));
-                ioctl(tmp, EVIOCGNAME(sizeof(tmpName)), tmpName);
-                printf("%s\n", tmpName);
-                if (strcmp(tmpName, "tahometer-keyhandler") == 0) {
-                    device = fullPath;
-                    break;
-                } else
-                    die("cant find taho");
+                device = fullPath;
             }
         }
     } while (dp != NULL);
@@ -104,39 +93,10 @@ int main(int argc, char *argv[]) {
         }
 
     }
-//    while (1) {
-//        if (read(evdev.fd, &ev, sizeof(struct input_event)) != -1) {
-//            FD_ZERO(&fds);
-//            maxfd = -1;
+//    printf("Exiting.\n");
 //
-//            FD_SET(evdev.fd, &fds);
-//            if (maxfd < evdev.fd) maxfd = evdev.fd;
+//    ioctl(evdev.fd, EVIOCGRAB, 0);
+//    close(evdev.fd);
 //
-//            result = select(maxfd + 1, &fds, NULL, NULL, NULL);
-//            if (result == -1) {
-//                die("");
-//            }
-//
-//
-//            if (!FD_ISSET(evdev.fd, &fds)) {
-//                die("");
-//            }
-//
-//            if ((rd = read(evdev.fd, ev, size * 64)) < size) {
-//                die("");
-//            }
-//
-//            numevents = rd / size;
-//            for (int j = 0; j < numevents; ++j) {
-//                printf("%s: Type[%d] Code[%d] Value[%d]\n", evdev.device, ev[j].type, ev[j].code, ev[j].value);
-//            }
-//        }
-//    }
-
-    printf("Exiting.\n");
-
-    ioctl(evdev.fd, EVIOCGRAB, 0);
-    close(evdev.fd);
-
-    return 0;
+//    return 0;
 }
